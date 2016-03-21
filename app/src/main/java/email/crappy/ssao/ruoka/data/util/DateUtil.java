b5570a09278e7a0bc19a0bed8267898652b50ada
Santeri Elo <me@santeri.xyz>
2016-03-21 15:34:49 +0200

    Added docs to DateUtil for clarity

--- app/src/main/java/email/crappy/ssao/ruoka/data/util/DateUtil.java
+++ app/src/main/java/email/crappy/ssao/ruoka/data/util/DateUtil.java
@@ -2,7 +2,6 @@ package email.crappy.ssao.ruoka.data.util;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 
 import timber.log.Timber;
 
@@ -10,11 +9,23 @@ import timber.log.Timber;
  * @author Santeri 'iffa'
  */
 public class DateUtil {
+    /**
+     * Checks if a given Date is "expired" == before the current Date
+     *
+     * @param date Date
+     * @return True if expired
+     */
     public static boolean isExpired(Date date) {
         Timber.i("Comparing %s with %s", date, Calendar.getInstance().getTime());
         return date.before(Calendar.getInstance().getTime());
     }
 
+    /**
+     * Checks if a given date (dd.MM) is today
+     *
+     * @param date Date (dd.MM)
+     * @return True if today
+     */
     public static boolean isToday(String date) {
         int day = Integer.parseInt(date.split("\\.")[0]);
         int month = Integer.parseInt(date.split("\\.")[1]);
